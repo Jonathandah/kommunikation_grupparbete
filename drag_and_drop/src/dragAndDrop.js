3891054d7a229819afefc6ef4437ad5470990fe0
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-01-25 18:02:51 +0100

    lagt till en ny css-fil och updaterade funktioner i js

--- drag_and_drop/src/dragAndDrop.js
+++ drag_and_drop/src/dragAndDrop.js
@@ -1,9 +1,15 @@
 let array_item =  document.querySelectorAll("li");
 console.log(array_item);
 for (let li of array_item){
+    li.setAttribute("draggable", "true");
+    li.addEventListener("dragstart", dragStart);
     li.addEventListener("mouseover", mouseOver);
 }
 
+function dragStart(e){
+    console.log("mouse start");
+    e.target.setAttribute("style", "opacity: 0.4;");
+}
 function mouseOver(){
     console.log("mouse over");
 
